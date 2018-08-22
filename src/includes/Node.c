5d28d268f80a5a60513c71e53dce95146ce8c073
Jacob Cassady <jtcass01@louisville.edu>
2018-08-21 20:11:20 -0500

    directory restructuring

---
+++ src/includes/Node.c
@@ -0,0 +1,23 @@
+
+/* Compiler/OS Headers */
+#include <stdlib.h>
+#include <stdio.h>
+/* User Created Headers */
+#include "../headers/Node.h"
+
+Node* createNode(int value, Node *previousNode){
+	Node *newNode = malloc(sizeof(Node));
+
+	newNode->value = value;
+	newNode->previousNode = previousNode;
+
+	return newNode;
+}
+
+void printNode(Node *node){
+	printf("Node value = %d", node->value);
+}
+
+void deleteNode(Node *node){
+	free(node);
+}
